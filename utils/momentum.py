5c689ea84499512f728de1b46a5b0243bfe18171
Semio Zheng <prairy.long@gmail.com>
2015-12-07 14:45:19 +0800

    added turunover

--- utils/momentum.py
+++ utils/momentum.py
@@ -13,7 +13,7 @@ import tushare as ts
 from datetime import datetime
 
 # 读入同花顺数据
-ths = pd.read_csv('./ths.csv', encoding='GB18030', na_values='--')
+ths = pd.read_csv('../data/ths.csv', encoding='GB18030', na_values='--')
 ths['date'] = datetime.today().date()
 ths['code'] = ths[u'代码'].apply(lambda x: x[2:])
 ths = ths[[ u'所属行业', u'流通股本',  u'code',  u'date']]
@@ -22,6 +22,7 @@ ths.columns = ['industry', 'outstanding', 'code', 'date']
 # 下载当前行情
 qs = ts.get_today_all()
 qs = qs.set_index('code')
+qs = qs.drop_duplicates()
 
 # 计算板块动量
 res = {}
@@ -30,17 +31,21 @@ for ind in ths['industry'].unique():
     df = ths[ths['industry'] == ind].copy()
     df['weight'] = df.outstanding / df.outstanding.sum()
     df = df.set_index('code')
-    
-    res[ind] = (df['weight'] * qs.ix[df.index][u'changepercent']).sum()
-    
-res = pd.DataFrame(res, index=['moment'])
-mo = res.T.sort_values(by='moment')
+
+    momt = (df['weight'] * qs.ix[df.index][u'changepercent']).sum()
+    turn = (df['weight'] * qs.ix[df.index][u'turnoverratio']).sum()
+    res[ind] = {'turnover': turn, 'momentum': momt}
+
+
+res = pd.DataFrame(res).T
+mo1 = res.sort_values(by='momentum').head(5)
+mo2 = res.sort_values(by='momentum', ascending=False).head(5)
 
 # end
 print "\n"
 
-print mo.head(5)
+print mo1.head(5)
 
 print "\n---\n"
 
-print mo.tail(5)
+print mo2.head(5)
